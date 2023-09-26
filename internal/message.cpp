7dcb4553e7201c6d221aff6840cb6774a1fcae32
MagicTINTIN <valentinservieres@gmail.com>
2023-09-27 00:21:00 +0300

    adding more class

--- internal/message.cpp
+++ internal/message.cpp
@@ -2,15 +2,18 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <vector>
 #include "../includes/nlohmann/json.hpp"
 #include "message.h"
 
 using json = nlohmann::json;
 
-Message::Message() : sender(""), content(""), dateTime(""), certifiedUser(0), rank(0)
+/* MESSAGE CLASS*/
+
+Message::Message() : sender(""), content(""), dateTime(""), certifiedUser(0), rank(0), deleted(false)
 {}
 
-Message::Message(std::string author, std::string message, std::string date, int certified, int rnk) : sender(author), content(message), dateTime(date), certifiedUser(certified), rank(rnk)
+Message::Message(std::string author, std::string message, std::string date, int certified, int rnk, bool del=false) : sender(author), content(message), dateTime(date), certifiedUser(certified), rank(rnk), deleted(del)
 {}
 
 void Message::print()
@@ -18,7 +21,25 @@ void Message::print()
     std::cout << "[" << sender << "] " << content << std::endl;
 }
 
-void setAllMessages(json messages) {
+bool Message::operator==(Message const &m) {
+    return this->sender == m.sender 
+        && this->content == m.content
+        && this->dateTime == m.dateTime
+        && this->certifiedUser == m.certifiedUser
+        && this->rank == m.rank;
+}
+
+/* MESSAGE MEMORY CLASS */
+
+MessageMemory::MessageMemory() : nbMessages(0), nbLastMessageFound(0)
+{
+}
+
+void MessageMemory::AddMessage(Message msg) {
+
+}
+
+void MessageMemory::setAllMessages(json messages) {
     for (const auto &msg : messages.items())
 	{
 		
--- internal/message.h
+++ internal/message.h
@@ -2,6 +2,8 @@
 #define MESSAGE_H_INCLUDED
 
 #include <string>
+#include <vector>
+#include "../includes/nlohmann/json.hpp"
 
 class Message
 {
@@ -11,11 +13,45 @@ private:
     std::string dateTime;
     int certifiedUser;
     int rank;
+    bool deleted;
 public:
     Message();
-    Message(std::string author, std::string message, std::string date, int certified, int rnk);
+    Message(std::string author, std::string message, std::string date, int certified, int rnk, bool del=false);
 
+    /**
+     * To print a message in terminal
+    */
     void print();
+    /**
+     * Set a message as deleted from the server
+    */
+    void deletedFromServer();
+    /**
+     * Compare messages
+    */
+    bool operator==(Message const &m);
 };
+
+class MessageMemory
+{
+private:
+    int nbMessages;
+    int nbLastMessageFound;
+    std::vector<Message> memory;
+public:
+    MessageMemory();
+    
+    /**
+     * Add message to the local memory
+    */
+    void AddMessage(Message msg);
+
+    /**
+     * Set all messages from server in memory
+    */
+    void setAllMessages(json messages);
+};
+
+
 
 #endif // MESSAGE_H_INCLUDED
