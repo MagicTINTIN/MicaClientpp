fe3de503974bf60c1c6190af4fb83e402f73d82f
MagicTINTIN <valentinservieres@gmail.com>
2023-09-28 00:24:42 +0300

    add some colors and html escape

--- internal/message.h
+++ internal/message.h
@@ -4,9 +4,31 @@
 #include <string>
 #include <time.h>
 
-time_t getTimestamp(std::string t);
-void cleanMessageList(std::string &s);
+//the following are UBUNTU/LINUX, and MacOS ONLY terminal color codes.
+#define RESET       "\033[0m"
+#define BLACK       "\033[30m"      /* Black */
+#define RED         "\033[31m"      /* Red */
+#define GREEN       "\033[32m"      /* Green */
+#define YELLOW      "\033[33m"      /* Yellow */
+#define BLUE        "\033[34m"      /* Blue */
+#define MAGENTA     "\033[35m"      /* Magenta */
+#define CYAN        "\033[36m"      /* Cyan */
+#define WHITE       "\033[37m"      /* White */
+#define BOLDBLACK   "\033[1m\033[30m"      /* Bold Black */
+#define BOLDRED     "\033[1m\033[31m"      /* Bold Red */
+#define BOLDGREEN   "\033[1m\033[32m"      /* Bold Green */
+#define BOLDYELLOW  "\033[1m\033[33m"      /* Bold Yellow */
+#define BOLDBLUE    "\033[1m\033[34m"      /* Bold Blue */
+#define BOLDMAGENTA "\033[1m\033[35m"      /* Bold Magenta */
+#define BOLDCYAN    "\033[1m\033[36m"      /* Bold Cyan */
+#define BOLDWHITE   "\033[1m\033[37m"      /* Bold White */
 
+time_t getTimestamp(std::string t);
+void ReplaceStringInPlace(std::string &s, const std::string &search, const std::string &replace);
+void escapeBackslash(std::string &s);
+char getCharacterFromEscapeSequence(const std::string& escapeSequence);
+void replaceEscapeSequences(std::string &s);
+void cleanMessageList(std::string &s);
 class Message
 {
 public:
